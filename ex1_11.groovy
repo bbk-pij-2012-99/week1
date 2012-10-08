eaba4c4582337f9d800b4ebb6a58667b3ddda617
Clare Matthews <clarematthews@gmail.com>
2012-10-08 19:50:40 +0100

    Correct typos on comments

--- ex1_11.groovy
+++ ex1_11.groovy
@@ -7,7 +7,7 @@ boolean sameSuit = false;
 boolean isStraight = false;
 String str;
 
-\\load cards
+//load cards
 
 str = System.console().readLine();
 if (str == "J") {
@@ -85,7 +85,7 @@ else {
 String suit5 = System.console().readLine();
 
 
-\\ Order values
+// Order values
 if (val1 > val2) {
 	min = val2;
 	val2 = val1;
@@ -147,13 +147,13 @@ else if (val5 < val4) {
 	val4 = min;
 }
 
-\\Check consecutive
+//Check consecutive
 
 if ( (val2 - val1 == 1) && (val3 - val2 == 1) && (val4 - val3 == 1) && (val5 - val4 == 1) {
 	isStraight = true;
 }
 	
-\\Check ranks
+//Check ranks
 
 if ((val1 == val4) || (val2 == val5)) {
 	maxRank = 4;
@@ -191,13 +191,13 @@ else if (val3 == val4 || val4 == val5) {
 
 
 
-\\Check suits
+//Check suits
 
 if ((suit1 == suit2) && (suit1 == suit3) && (suit1 == suit4) && (suit1 == suit5) {
 	sameSuit = true;
 }
 
-\\Choose best hand
+//Choose best hand
 
 println "Best hand:"
 if (isStraight && sameSuit) {
